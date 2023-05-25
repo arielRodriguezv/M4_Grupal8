5ed80a1dd1deead33c1bd4260e611c7386aa6585
arielrodriguez <ariel.rodriguezv@gmail.com>
2023-05-24 21:52:26 -0400

    segundo commit

--- M4Ejercicio_Grupal8/src/M4Ejercicio_Grupal8.java
+++ M4Ejercicio_Grupal8/src/M4Ejercicio_Grupal8.java
@@ -1,8 +1,35 @@
 
+import java.util.Scanner;
 public class M4Ejercicio_Grupal8 {
+	
+	static void registrar() {
+		
+		Scanner name = new Scanner(System.in);
+		System.out.println("Ingrese nombre");
+		nombre = name.nextLine();
+		
+		Scanner birth = new Scanner(System.in);
+		System.out.println("Ingrese nombre");
+		String fechaNac = birth.nextLine();
+		
+		Scanner id = new Scanner(System.in);
+		System.out.println("Ingrese nombre");
+		String run = id.nextLine();
+		
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String nombre;
+		
+		
+		System.out.println("Asesorias en prevecion de riesgos SPA.");
+		System.out.println("Bienvenido");
+		
+		registrar();
+		
+		System.out.println();
 
 	}
 
